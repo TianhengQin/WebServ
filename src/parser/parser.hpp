8ced487ed542839265f0a76e33dfd2bb2116f764
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2024-03-11 13:13:13 +0100

    parser class

---
+++ src/parser/parser.hpp
@@ -0,0 +1,16 @@
+
+#ifndef PARSER_HPP
+# define PARSER_HPP
+
+# include <map>
+
+class Parser {
+private:
+    std::map<std::string, std::string> mp;
+
+public:
+    Parser();
+    ~Parser();
+};
+
+#endif
