eaffa531c84f331ef93ff6191673af009648bcea
TianhengQin <tianhengchin@gmail.com>
2024-04-05 18:00:50 +0200

    log update

--- Log/Log.cpp
+++ Log/Log.cpp
@@ -6,12 +6,20 @@ void Log::print(int level, std::string const &msg, unsigned long i) {
         return;
     }
     if (level == DEBUG) {
-        std::cout << "   [DEBUG]   " << msg << i << std::endl;
+        std::cout << "   [DEBUG]   " << msg;
+        if (i) std::cout << i;
+        std::cout << std::endl;
     } else if (level == INFO) {
-        std::cout << "   [INFO]    " << msg << i << std::endl;
+        std::cout << "   [INFO]    " << msg;
+        if (i) std::cout << i;
+        std::cout << std::endl;
     } else if (level == WARNING) {
-        std::cerr << "  ![WARNING] " << msg << i << std::endl;
+        std::cerr << "  ![WARNING] " << msg;
+        if (i) std::cerr << i;
+        std::cerr << std::endl;
     } else if (level == ERROR) {
-        std::cerr << "!!![ERROR]!! " << msg << i << std::endl;
+        std::cerr << "!!![ERROR]!! " << msg;
+        if (i) std::cerr << i;
+        std::cerr << std::endl;
     }
 }
