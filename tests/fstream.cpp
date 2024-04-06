ee1f969deef2a4a1c086ee0ab372789ffbcfd72f
Tianheng Qin <tqin@i2m.speedport.ip>
2024-04-06 17:46:24 +0200

    test bin file i/o

---
+++ tests/fstream.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+int main () 
+{
+    std::ifstream f1 ("test_picture.jpeg", std::fstream::binary);
+
+    std::ofstream f2 ("test_picture_copy.jpeg", std::fstream::trunc | std::fstream::binary);
+
+    std::string bf;
+
+    std::ostringstream ss;
+	ss << f1.rdbuf();
+    bf = ss.str();
+
+    std::cout << bf << std::endl;
+
+    f2 << bf;
+
+
+    f1.close();
+    f2.close();
+    return 0;
+}
