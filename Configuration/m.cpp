d0a5ee8e42edf349015604aa8fddf40e8c375f36
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2024-04-20 05:56:37 +0200

    Testing Parsing

--- Configuration/m.cpp
+++ Configuration/m.cpp
@@ -7,13 +7,63 @@
 #include <memory>
 #include <fstream>
 #include <stack>
+#include <algorithm>
+#include <cctype>
+#include <locale>
+
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+}
 
 
+std::pair<std::string, std::vector<std::string> > parseNameAndArguments(const std::string& line) {
+    std::pair<std::string, std::vector<std::string> > result;
+    std::string::size_type                           pos;
+    std::string                                     name;
+    std::vector<std::string>                        arguments;
 
+    pos = line.find_first_of(" \t");
+    if (pos == std::string::npos) {
+        name = line;
+    } else {
+        name = line.substr(0, pos);
+        while (pos != std::string::npos) {
+            std::string::size_type   nextPos;
+
+            pos = line.find_first_not_of(" \t", pos);
+            if (pos == std::string::npos) break;
+
+            nextPos = line.find_first_of(" \t", pos);
+            arguments.push_back(line.substr(pos, nextPos - pos));
+            pos = nextPos;
+        }
+    }
+
+    result.first = name;
+    result.second = arguments;
+    return result;
+}
 
 // Trim whitespace from both ends of a string
 void parseConfig(std::istream& input) {
-    std::stack<std::shared_ptr<Block> > blockStack;
+    Block *root = new Block("root", {});
+    std::stack< Block * > blockStack;
     std::string line;
 
     while (getline(input, line)) {
@@ -25,13 +75,20 @@ void parseConfig(std::istream& input) {
             Block                                               *block;
 
             blockNameArgs = parseNameAndArguments(line.substr(0, line.size() - 1));
-            block = new Block(directiveNameArgs.first, directiveNameArgs.second);
+            block = new Block(blockNameArgs.first, blockNameArgs.second);
             
             if (!blockStack.empty()) {
                 blockStack.top()->addChild(block);
             }
             blockStack.push(block);
         } else if (line == "}") {
+            // Debugging
+            if (blockStack.empty()) {
+                std::cerr << "Unmatched '}'" << std::endl;
+                continue;
+            } else if (blockStack.size() == 1) {
+                root->addChild(blockStack.top());
+            }
             blockStack.pop();
         } else {
             std::pair<std::string, std::vector<std::string> >   directiveNameArgs;
@@ -45,6 +102,8 @@ void parseConfig(std::istream& input) {
             }
         }
     }
+
+    root->print();
 }
 
 
@@ -62,8 +121,8 @@ int main(int argc, char** argv) {
     }
 
     parseConfig(input);
-
-
     return 0;
 }
+
+
 
