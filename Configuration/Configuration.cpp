5b24d5e18641de74252fee3bb3fbf78cb5afc08e
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2024-05-23 01:11:11 +0200

    Configuration: Copy assignment, overloading << , and deleting comments.

--- Configuration/Configuration.cpp
+++ Configuration/Configuration.cpp
@@ -6,57 +6,7 @@
  * 
  */
 
-Configuration::Configuration(void) {
-	// Server s1;
-	// set_server_context(s1);
-	// s1.setHost("127.0.0.1");
-	// s1.setPort(8080);
-	// s1.addServerName("127.0.0.1");
-	// s1.setErrorPage(301, "./websites/err_page/301.html");
-	// s1.setErrorPage(404, "./websites/err_page/404.html");
-	// s1.setClientMaxBodySize(1024*1024);
-	// s1.setRoot("./websites");
-	
-	// Location l1;
-	// set_location_context(s1, l1);
-	// l1.setPath("/");
-	// l1.setRoot("/HTML");
-	// l1.setAllowedMethods(GET|POST|DELETE);
-	// l1.setRedir("/HTML/index.html");
-	// l1.setCgi(".php", "/usr/bin/php");
-	// l1.setCgi(".sh", "/bin/bash");
-	// l1.setCgi(".py", "/usr/local/bin/python3");
-	// Location l2;
-	// set_location_context(s1, l2);
-	// l2.setPath("/test");
-	// l2.setAllowedMethods(GET|PUT|DELETE);
-	// l2.setRoot("/HTML/test_page");
-	// l2.setAutoindex(true);
-	// l2.addIndex("/HTML/index.html");
-	// s1.addLocation(l1);
-	// s1.addLocation(l2);
-	// _servers.push_back(s1);
-
-	// Server s2;
-	// set_server_context(s2);
-	// s2.setHost("127.0.0.1");
-	// s2.setPort(8080);
-	// s2.addServerName("localhost");
-	// s2.setRoot("./websites");
-	// set_location_context(s2, l1);
-	// s2.addLocation(l1);
-	// _servers.push_back(s2);
-
-	// Server s3;
-	// set_server_context(s3);
-	// s3.setHost("127.0.0.1");
-	// s3.setPort(8081);
-	// s3.addServerName("localhost");
-	// s3.setRoot("./websites");
-	// set_location_context(s3, l1);
-	// s3.addLocation(l1);
-	// _servers.push_back(s3);
-}
+Configuration::Configuration(void) { }
 
 
 Configuration::Configuration(const Configuration &other) {
@@ -65,13 +15,22 @@ Configuration::Configuration(const Configuration &other) {
 
 Configuration &Configuration::operator=(const Configuration &other) {
 	if (this != &other) {
-		// Todo: deep copy
+		this->_filename = other._filename;
+		this->_parser = other._parser;
+		this->_root = other._root;
+		this->_index = other._index;
+		this->_error_page = other._error_page;
+		this->_client_max_body_size = other._client_max_body_size;
+		this->_allow_methods = other._allow_methods;
+		this->_autoindex = other._autoindex;
+		this->_servers = other._servers;
+		this->_cgi = other._cgi;
 	}
 	return (*this);
 }
 
 Configuration::~Configuration() {
-	// delete _root;
+	delete this->_parser;
 }
 
 
@@ -82,25 +41,27 @@ Configuration::Configuration(std::string filename) : _filename(filename) {
 	this->_client_max_body_size = 1024 * 1024;
 	this->_allow_methods = GET | POST | DELETE | PUT | HEAD;
 	this->_autoindex = false;
-	// this->_parser = NginxParser(filename);
 
-	// this->_parser.parse_configuration_file();
-	parse_configuration_file();
-	// process_http_block(this->_parser.getHttpBlock());
+	// this->_parser = NginxParser(filename);
+	this->parse_configuration_file();
 }
 
-// void	Configuration::parse_configuration_file(std::ifstream &file) {
 void	Configuration::parse_configuration_file(void) {
-	std::ifstream file(this->_filename.c_str());
+	Block			*httpBlock;
+	std::ifstream	file(this->_filename.c_str());
+
 	if (!file.is_open()) {
 		throw std::runtime_error("Error opening file: " + this->_filename);
 	}
-
 	this->_parser = new NginxParser(file);
-	Block *httpBlock = this->_parser->getHttpBlock();
-	// std::cout << *httpBlock << std::endl;
-	process_http_block(httpBlock);
+	httpBlock = this->_parser->getHttpBlock();
+	// std::cout << *httpBlock << std::endl; // Debug
 
+	try {
+		this->process_http_block(httpBlock);
+	} catch (std::exception &e) {
+		throw std::runtime_error("Error processing configuration file: " + this->_filename);
+	}
 }
 
 void Configuration::process_http_block(Block *httpBlock) {
@@ -108,6 +69,7 @@ void Configuration::process_http_block(Block *httpBlock) {
 	std::vector<ASTNode *>::iterator	child;
 	Block 								*serverBlock;
 	Directive							*directive;
+	std::string							name;
 	std::vector<std::string>			args;
 
 	blockChildren = httpBlock->getChildren();
@@ -115,26 +77,26 @@ void Configuration::process_http_block(Block *httpBlock) {
 		directive = dynamic_cast<Directive*>(*child);
 		if (!directive)
 			continue;
+		name = directive->getName();
 		args = directive->getArguments();
-		if (directive->getName() == "root") {
+		if (name == "root") {
 			this->_root = args[0];
-		} else if (directive->getName() == "index") {
+		} else if (name == "index") {
 			this->_index = args[0];
-		} else if (directive->getName() == "error_page") {
+		} else if (name == "error_page") {
 			if (args.size() == 2) {
 				this->_error_page[std::stoi(args[0])] = args[1];
 			} else if (args.size() == 1) {
 				this->_error_page[std::stoi(args[0])] = "";
 			}
-		} else if (directive->getName() == "client_max_body_size") {
+		} else if (name == "client_max_body_size") {
 			this->_client_max_body_size = parseSize(args[0]);
-		// } else if (directive->getName() == "limit_except") { // Real `limit_except` directive is in location block
-		} else if (directive->getName() == "allow_methods") {
+		} else if (name == "allow_methods") { // || name == "limit_except") {
 			this->_allow_methods = parseMethods(args);
-		} else if (directive->getName() == "autoindex") {
+		} else if (name == "autoindex") {
 			if (args[0] == "on") {
 				this->_autoindex = true;
-			} else {
+			} else if (args[0] == "off") {
 				this->_autoindex = false;
 			}
 		}
@@ -143,19 +105,8 @@ void Configuration::process_http_block(Block *httpBlock) {
 		serverBlock = dynamic_cast<Block*>(*child);
 		if (serverBlock && serverBlock->getName() == "server") {
 			Server serv;
-			set_server_context(serv);
-			// serv.setRoot(this->_root);
-			// serv.addIndex(this->_index);
-			// serv.setAllowedMethods(this->_allow_methods);
-			// for (std::map<int, std::string>::iterator it = this->_error_page.begin(); it != this->_error_page.end(); ++it) {
-			// 	serv.setErrorPage(it->first, it->second);
-			// }
-			// serv.setClientMaxBodySize(this->_client_max_body_size);
-			// serv.setAutoindex(this->_autoindex);
-			// for (std::map<std::string, std::string>::iterator it = this->_cgi.begin(); it != this->_cgi.end(); ++it) {
-			// 	serv.setCgi(it->first, it->second);
-			// }
 
+			set_server_context(serv);
 			process_server_block(serverBlock, serv);
 			this->_servers.push_back(serv);
 		}
@@ -167,44 +118,44 @@ void Configuration::process_server_block(Block *serverBlock, Server &server) {
 	std::vector<ASTNode *>::iterator	child;
 	Block								*locationBlock;
 	Directive							*directive;
+	std::string							name;
 	std::vector<std::string>			args;
 
 	blockChildren = serverBlock->getChildren();
 	for (child = blockChildren.begin(); child != blockChildren.end(); ++child) {
 		directive = dynamic_cast<Directive*>(*child);
 		if (directive) {
+			name = directive->getName();
 			args = directive->getArguments();
-			if (directive->getName() == "listen") {
+			if (name == "listen") {
 				process_listen_directive(args, server);
-			} else if (directive->getName() == "server_name") {
+			} else if (name == "server_name") {
 				for (std::vector<std::string>::iterator it = args.begin(); it != args.end(); ++it) {
 					server.addServerName(*it);
 				}
-				// server.addServerName(args[0]);
-			} else if (directive->getName() == "root") {
+			} else if (name == "root") {
 				server.setRoot(args[0]);
-			} else if (directive->getName() == "index") {
+			} else if (name == "index") {
 				for (std::vector<std::string>::iterator it = args.begin(); it != args.end(); ++it) {
 					server.addIndex(*it);
 				}
-				// server.addIndex(args[0]);
-			} else if (directive->getName() == "allow_methods") {
+			} else if (name == "allow_methods") {
 				server.setAllowedMethods(parseMethods(args));
-			} else if (directive->getName() == "error_page") {
+			} else if (name == "error_page") {
 				if (args.size() == 2) {
 					server.setErrorPage(std::stoi(args[0]), args[1]);
 				} else if (args.size() == 1) {
 					server.setErrorPage(std::stoi(args[0]), "");
 				}
-			} else if (directive->getName() == "client_max_body_size") {
+			} else if (name == "client_max_body_size") {
 				server.setClientMaxBodySize(parseSize(args[0]));
-			} else if (directive->getName() == "autoindex") {
+			} else if (name == "autoindex") {
 				if (args[0] == "on") {
 					server.setAutoindex(true);
 				} else if (args[0] == "off") {
 					server.setAutoindex(false);
 				}
-			} else if (directive->getName() == "cgi") {
+			} else if (name == "cgi") {
 				try {
 					server.setCgi(args[0], args[1]);
 				} catch (std::exception &e) {
@@ -219,19 +170,6 @@ void Configuration::process_server_block(Block *serverBlock, Server &server) {
 			Location loc;
 			set_location_context(server, loc);
 			loc.setPath(locationBlock->getArguments()[0]);
-			// loc.setRoot(server.getRoot());
-			// loc.addIndex(server.getIndex());
-			// loc.setAllowedMethods(server.getAllowedMethods());
-			// loc.setAutoindex(server.getAutoindex());
-			// loc.setClientMaxBodySize(server.getClientMaxBodySize());
-			// std::map<int, std::string> errorPages = server.getErrorPages();
-			// for (std::map<int, std::string>::iterator it = errorPages.begin(); it != errorPages.end(); ++it) {
-			// 	loc.setErrorPage(it->first, it->second);
-			// }
-			// std::map<std::string, std::string> cgi = server.getCgi();
-			// for (std::map<std::string, std::string>::iterator it = cgi.begin(); it != cgi.end(); ++it) {
-			// 	loc.setCgi(it->first, it->second);
-			// }
 			process_location_block(locationBlock, loc);
 			server.addLocation(loc);
 		}
@@ -244,38 +182,40 @@ void Configuration::process_location_block(Block *locationBlock, Location &locat
 	std::vector<ASTNode *>				blockChildren;
 	std::vector<ASTNode *>::iterator	child;
 	Directive							*directive;
+	std::string							name;
 	std::vector<std::string>			args;
 
 	blockChildren = locationBlock->getChildren();
 	for (child = blockChildren.begin(); child != blockChildren.end(); ++child) {
 		directive = dynamic_cast<Directive*>(*child);
 		if (directive) {
+			name = directive->getName();
 			args = directive->getArguments();
-			if (directive->getName() == "root") {
+			if (name == "root") {
 				location.setRoot(args[0]);
-			} else if (directive->getName() == "index") {
+			} else if (name == "index") {
 				for (std::vector<std::string>::iterator it = args.begin(); it != args.end(); ++it) {
 					location.addIndex(*it);
 				}
-			} else if (directive->getName() == "error_page") {
+			} else if (name == "error_page") {
 				if (args.size() == 2) {
 					location.setErrorPage(std::stoi(args[0]), args[1]);
 				} else if (args.size() == 1) {
 					location.setErrorPage(std::stoi(args[0]), "");
 				}
-			} else if (directive->getName() == "allow_methods"){ //} || directive->getName() == "limit_except") {
+			} else if (name == "allow_methods"){ //} || name == "limit_except") {
 				location.setAllowedMethods(parseMethods(args));
-			} else if (directive->getName() == "client_max_body_size") {
+			} else if (name == "client_max_body_size") {
 				location.setClientMaxBodySize(parseSize(args[0]));
-			} else if (directive->getName() == "return") {
+			} else if (name == "return") {
 				location.setRedir(args[0]);
-			} else if (directive->getName() == "autoindex") {
+			} else if (name == "autoindex") {
 				if (args[0] == "on") {
 					location.setAutoindex(true);
 				} else if (args[0] == "off") {
 					location.setAutoindex(false);
 				}
-			} else if (directive->getName() == "cgi") {
+			} else if (name == "cgi") {
 				try {
 					location.setCgi(args[0], args[1]);
 				} catch (std::exception &e) {
@@ -319,9 +259,9 @@ void Configuration::set_location_context(Server &server, Location &location) {
 
 
 void Configuration::process_listen_directive(std::vector<std::string> &args, Server &server) {
-	std::string	host = "0.0.0.0";
-	int			port = 80;
-	bool		isDefaultServer = false;
+	std::string		host = "0.0.0.0";
+	int				port = 80;
+	bool			isDefaultServer = false;
 
 	for (std::vector<std::string>::iterator it = args.begin(); it != args.end(); ++it) {
 		if (*it == "default_server") {
@@ -379,6 +319,7 @@ unsigned int Configuration::parseSize(std::string sizeStr) {
 
 unsigned int Configuration::parseMethods(std::vector<std::string> &methods) {
 	unsigned int methodFlags = 0;
+
 	for (std::vector<std::string>::iterator method = methods.begin(); method != methods.end(); ++method) {
 		std::transform(method->begin(), method->end(), method->begin(), ::toupper);
 
@@ -397,8 +338,9 @@ unsigned int Configuration::parseMethods(std::vector<std::string> &methods) {
 	return (methodFlags);
 }
 
-/* Getters */
 
+
+/* Getters */
 
 std::string Configuration::getFilename(void) { return (this->_filename); }
 
@@ -418,15 +360,25 @@ bool Configuration::getAutoindex(void) { return (this->_autoindex); }
 
 std::map<std::string, std::string> Configuration::getCgi(void) { return (this->_cgi); }
 
-std::vector<Server> Configuration::getServers(void) {
-	return (this->_servers);
-}
+std::vector<Server> Configuration::getServers(void) { return (this->_servers); }
+
 
 
 /* Operator overloads */
 
 std::ostream &operator<<(std::ostream &out, Configuration &config) {
 	out << "Configuration file: " << config.getFilename() << std::endl;
+	out << "Root: " << config.getRoot() << std::endl;
+	out << "Index: " << config.getIndex() << std::endl;
+	out << "Error Pages: " << std::endl;
+	std::map<int, std::string> errorPages = config.getErrorPages();
+	for (std::map<int, std::string>::iterator it = errorPages.begin(); it != errorPages.end(); ++it) {
+		out << "  " << it->first << " " << it->second << std::endl;
+	}
+	out << "Client Max Body Size: " << config.getClientMaxBodySize() << std::endl;
+	out << "Allowed Methods: " << config.getAllowedMethods() << std::endl;
+	out << "Autoindex: " << config.getAutoindex() << std::endl;
+	out << "CGI: " << std::endl;
 	out << "Servers: " << std::endl;
 	std::vector<Server> servs = config.getServers();
 	for (std::vector<Server>::iterator serv = servs.begin(); serv != servs.end(); ++serv) {
--- main.cpp
+++ main.cpp
@@ -10,22 +10,6 @@
 # include "Configuration.hpp"
 # include "Location.hpp"
 
-// int main(int argc, char **argv) {
-//	try {
-//		if (argc != 2) {
-//		    throw std::runtime_error("Usage: " + std::string(argv[0]) + " <config_file>");//throw invalid_argument
-//		}
-//		Configuration config(argv[1]);
-//		// std::cout << "Configuration file: " << std::endl;
-//		// WebServ web_serv(config);
-//		// web_serv.run();
-//	} catch (std::exception &e) {
-//		std::cerr << "Error: " << e.what() << std::endl;//Log::print
-//		return 1;
-//	}
-//	return 0;
-// }
-
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
