41492d045e9b39b21c20e7b16f5c2276ba0f468b
TianhengQin <tianhengchin@gmail.com>
2024-05-27 00:39:21 +0200

    memory leak

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char **argv) {
 	}
 
 	int watch_dog = 1000;
+
 	while (--watch_dog) {
 		try {
 			Log::print(INFO, "Server Restarting ...", 0);
@@ -25,8 +26,12 @@ int main(int argc, char **argv) {
 			Configuration config(argv[1]);
 
 			WebServ web_serv(config);
+			// throw std::runtime_error("test leak");
 			web_serv.run();
+
 		} catch (std::exception &e) {
+			// system("leaks webserv");
+			// exit(1);
 			Log::print(ERROR, e.what(), 0);
 			Log::print(WARNING, "Watch Dog ", watch_dog);
 		}
